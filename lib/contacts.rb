32a94e7b192c42f5ef2cfb3c2738d1148e0a90e0
Jake Sleman <jakesleman@icloud.com>
2020-05-17 22:33:21 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,3 +1,4 @@
+  
 require 'pry'
 
   # This is the array we will be passing into the remove_strawberry method
@@ -15,5 +16,15 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+contacts.each do |name, data|
+  if name == "Freddy Mercury"
+    data.each do |attribute, value|
+    if attribute == :favorite_ice_cream_flavors
+      value.delete_if {|ice_cream| ice_cream == "strawberry"}
+      data
+    #binding.pry
+  end
+end
+end
+end
 end
